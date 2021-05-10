5de7ca515b632b4590cc2114ee6f17dc009d0f88
yevhenboiev <yevhenboiev@gmail.com>
2021-05-10 10:48:40 +0300

    update GuessNumber

--- Lesson1/GuessNumber.java
+++ Lesson1/GuessNumber.java
@@ -1,18 +1,18 @@
 public class GuessNumber {
     public static void main(String[] args) {
-    int randomNumber = 10;
-    int myNumber;
+        int randomNumber = 10;
+        int myNumber;
 
-    for (int i = 0; i <= randomNumber; i++) {
-    myNumber = i;
-        if (randomNumber == myNumber) {
-            System.out.println("Поздравляю, число угадано!");
-            continue;
-        } else if (randomNumber < myNumber) {
-            System.out.println("Данное число больше того, что загадал компьютер");
-        } else if (randomNumber > myNumber) {
-            System.out.println("Данное число меньше того, что загадал компьютер");
+        for (int i = 0; i <= randomNumber; i++) {
+            myNumber = i;
+            if (myNumber == randomNumber) {
+                System.out.println("Поздравляю, число угадано!");
+                break;
+            } else if (myNumber < randomNumber) {
+                System.out.println("Данное число больше того, что загадал компьютер");
+            } else if (myNumber > randomNumber) {
+                System.out.println("Данное число меньше того, что загадал компьютер");
+            }
         }
-    }
     }
 }
