ab7d69b4a2d392df264874b56f05c5efe752c2cf
yevhenboiev <yevhenboiev@gmail.com>
2021-05-16 22:11:08 +0300

    add Player.java

--- Lesson_2/Player.java
+++ Lesson_2/Player.java
@@ -7,15 +7,15 @@ public class Player {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public int getNumber() {
         return number;
     }
 
     public void setNumber(int number) {
         this.number = number;
-    }
-
-    public String getName() {
-        return name;
     }
 }
