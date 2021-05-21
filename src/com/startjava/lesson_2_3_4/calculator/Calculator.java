20cf43633d493b59dde66443a8d47a7ce3bc56b2
yevhenboiev <yevhenboiev@gmail.com>
2021-05-21 15:00:05 +0300

    Update Calculator

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,45 +1,36 @@
 package com.startjava.lesson_2_3_4.calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
 
-    private String mathExpression;
-    private String[] arrayMathExpression;
     private int firstNumber;
     private String sign;
     private int secondNumber;
 
-    public Calculator(String mathExpression) {
-        this.mathExpression = mathExpression;
+    Scanner console = new Scanner(System.in);
+
+    public Calculator() {
+        splitExpression(console.nextLine());
+        System.out.println(calculate());
     }
 
-    public void mathExpression() {
-        arrayMathExpression = mathExpression.split(" ");
+    private int calculate() {
+        return switch (sign) {
+            case "+" -> firstNumber + secondNumber;
+            case "-" -> firstNumber - secondNumber;
+            case "*" -> firstNumber * secondNumber;
+            case "/" -> firstNumber / secondNumber;
+            case "%" -> firstNumber % secondNumber;
+            case "^" -> (int) (Math.pow(firstNumber, secondNumber));
+            default -> 0;
+        };
+    }
+
+    private void splitExpression(String mathExpression) {
+        String[] arrayMathExpression = mathExpression.split(" ");
         firstNumber = Integer.parseInt(arrayMathExpression[0]);
         sign = arrayMathExpression[1];
         secondNumber = Integer.parseInt(arrayMathExpression[2]);
     }
-
-    public void calculate() {
-        int result = 0;
-        switch (sign) {
-            case "+" :
-                result = firstNumber + secondNumber;
-                break;
-            case "-" :
-                result = firstNumber - secondNumber;
-                break;
-            case "*" :
-                result = firstNumber * secondNumber;
-                break;
-            case "/" :
-                result = firstNumber / secondNumber;
-                break;
-            case "%" :
-                result = firstNumber % secondNumber;
-                break;
-            case "^" :
-                result = (int) (Math.pow(firstNumber, secondNumber));
-                }
-        System.out.println("Ваше выражение = " + result);
-        }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -3,18 +3,16 @@ package com.startjava.lesson_2_3_4.calculator;
 import java.util.Scanner;
 
 public class CalculatorTest {
-    
+
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         System.out.println("Добро пожаловать в калькулятор");
         String reply;
         do {
             System.out.print("Введите математическое выражение(Пример: \"2 + 3\"): ");
-            Calculator calculator = new Calculator(console.nextLine());
-            calculator.mathExpression();
-            calculator.calculate();
+            Calculator calculator = new Calculator();
             do {
-                System.out.println("Хотите продолжить вычисления? [yes/no]:");
+                System.out.print("Хотите продолжить вычисления? [yes/no]:");
                 reply = console.next();
             } while (!reply.equals("yes") && !reply.equals("no"));
         } while (reply.equals("yes"));
